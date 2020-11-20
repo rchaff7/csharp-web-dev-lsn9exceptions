d0a0c2fac7935f213569dbcbeeb38a4720eb8567
Ryan Chaffee <rchaff7@yahoo.com>
2020-11-20 00:32:29 -0600

    Completed

--- csharp-web-dev-lsn9exceptions/Program.cs
+++ csharp-web-dev-lsn9exceptions/Program.cs
@@ -7,18 +7,43 @@ namespace csharp_web_dev_lsn9exceptions
     {
         static double Divide(double x, double y)
         {
-            // Write your code here!
+            if (y == 0)
+            {
+                throw new ArgumentOutOfRangeException("Cannot divide by zero.");
+            }
+
+            return x / y;
         }
 
         static int CheckFileExtension(string fileName)
         {
-            // Write your code here!
+            if(fileName == null || fileName == "")
+            {
+                throw new ArgumentException("Filename submission cannot be empty or blank.");
+            }
+
+            if(fileName.EndsWith(".cs"))
+            {
+                return 1;
+            }
+
+            return 0;
         }
 
 
         static void Main(string[] args)
         {
             // Test out your Divide() function here!
+            try
+            {
+                Divide(2, 0);
+            }
+            catch(ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.WriteLine(Divide(10, 2));
+
 
             // Test out your CheckFileExtension() function here!
             Dictionary<string, string> students = new Dictionary<string, string>();
@@ -27,7 +52,17 @@ namespace csharp_web_dev_lsn9exceptions
             students.Add("Elizabeth", "MyCode.cs");
             students.Add("Stefanie", "CoolProgram.cs");
 
-
+            foreach (KeyValuePair<string, string> pair in students)
+            {
+                try
+                {
+                    Console.WriteLine(pair.Key + " points: " + CheckFileExtension(pair.Value));
+                }
+                catch(ArgumentException e)
+                {
+                    Console.WriteLine(pair.Key + ": " + e.Message);
+                }   
+            }
         }
     }
 }
